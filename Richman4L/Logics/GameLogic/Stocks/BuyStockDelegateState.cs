5e400531f25b228c40b2de3c7f3edd748d91bccb
Wencey Wang <wencey@outlook.com>
2016-06-30 15:50:18 +0800

    State

---
+++ Richman4L/Logics/GameLogic/Stocks/BuyStockDelegateState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System . Collections . Generic;
+using System . Linq;
+using System . Text;
+
+namespace WenceyWang . Richman4L . Stocks
+{
+	/// <summary>
+	/// 表示购买股票委托的结果
+	/// </summary>
+	public enum BuyStockDelegateState
+	{
+		/// <summary>
+		/// 委托尚未被执行。
+		/// </summary>
+		Waiting,
+
+		/// <summary>
+		/// 委托已经完成。
+		/// </summary>
+		Completed,
+
+		/// <summary>
+		/// 委托没有全部完成，由于玩家的现金不够。
+		/// </summary>
+		MoneyNotEnough,
+
+		/// <summary>
+		/// 委托没有全部完成，由于交易量不够。
+		/// </summary>
+		ValueNotEnough,
+
+		/// <summary>
+		/// 委托完全没有完成，由于股票自身原因（不交易，Buff）。
+		/// </summary>
+		StockCannotBuy,
+
+	}
+}
